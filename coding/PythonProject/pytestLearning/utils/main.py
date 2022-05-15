abf20f242e562cafbd6e0a21fd5df98b5749d6af
lili <1115665126@qq.com>
2022-05-15 18:35:35 +0800

    update main.py

--- coding/PythonProject/pytestLearning/utils/main.py
+++ coding/PythonProject/pytestLearning/utils/main.py
@@ -3,6 +3,8 @@
 # Press Alt+Shift+X to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pytest
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +13,4 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pytest.main(["-s", "-v", "--alluredir=../Outputs/allure"])  # allure文件生成的目录
